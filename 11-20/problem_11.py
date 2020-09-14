ed9f3ea8923442d7ea06bb6c65c1a5ea03f4812d
Jon Herbst <herbtaculargames@gmail.com>
2020-09-14 12:21:13 +0100

    Problem 11 final solution with comments regarding readability, code re-use and variable naming conventions

--- 11-20/problem_11.py
+++ 11-20/problem_11.py
@@ -1,12 +1,33 @@
 import os
 
 class LargestGridProduct:
+    """
+    Decided to scan the 20x20 grid by reading
+    the horizontal, vertical and diagonal
+    subsets separately as I felt it
+    was better for understanding
+    what is going on.
+
+    Kept the grid of numbers in a separate
+    file in a similar vein to that of
+    problem 8, as I felt it was
+    unnecessary to put that
+    in a variable.
+
+    Yes, you could probably combine all of the
+    looping elements in one function and
+    prevent re-scanning of the grid
+    I think you would sacrifice
+    too much readability for
+    a small gain.
+    """
     def __init__(self, directory):
         self.largest_product = 0
         self.digits = self.loadFile(directory)
 
         self.readHorizontal()
         self.readVertical()
+        self.readDiagonal()
 
     def loadFile(self, directory):
         numbers = []
@@ -22,13 +43,7 @@ class LargestGridProduct:
             index = 0
 
             while len(row) > (index+3):
-                product = 1
-
-                for item in row[index:index+4]:
-                    product *= int(item)
-
-                if self.largest_product < product:
-                    self.largest_product = product
+                self.checkProduct(row[index:index+4])
 
                 index += 1
 
@@ -38,7 +53,6 @@ class LargestGridProduct:
 
         while 20 > h_index:
             while 20 > (v_index+3):
-                product = 1
                 subset = [
                     self.digits[v_index][h_index],
                     self.digits[v_index+1][h_index],
@@ -46,16 +60,72 @@ class LargestGridProduct:
                     self.digits[v_index+3][h_index]
                 ]
 
-                for item in subset:
-                    product *= int(item)
-
-                if self.largest_product < product:
-                    self.largest_product = product
+                self.checkProduct(subset)
 
                 v_index += 1
             h_index += 1
             v_index = 0
-        return
+
+    def readDiagonal(self):
+        """
+        A rethink of the variable naming
+        convention could be beneficial
+        here to promote understanding
+        of the problem. I think it
+        obfuscates some elements
+        of the grid scanning.
+        """
+        v_index = 0
+        h_index = 0
+
+        while 20 > (v_index+3):
+            """Left-to-right diagonal scanning."""
+            while 20 > (h_index+3):
+                subset = [
+                    self.digits[v_index][h_index],
+                    self.digits[v_index+1][h_index+1],
+                    self.digits[v_index+2][h_index+2],
+                    self.digits[v_index+3][h_index+3]
+                ]
+
+                self.checkProduct(subset)
+            
+                h_index += 1
+            v_index += 1
+            h_index = 0
+
+        v_index = 0
+        h_index = 19
+
+        while 0 <= (h_index-3):
+            """Right-to-left diagonal scanning."""
+            while 20 > (v_index+3):
+                subset = [
+                    self.digits[v_index][h_index],
+                    self.digits[v_index+1][h_index-1],
+                    self.digits[v_index+2][h_index-2],
+                    self.digits[v_index+3][h_index-3]
+                ]
+
+                self.checkProduct(subset)
+            
+                v_index += 1
+            h_index -= 1
+            v_index = 0
+
+    def checkProduct(self, subset):
+        """
+        Avoided code duplication by creating
+        separate function when checking
+        the product of grid subsets.
+        """
+        product = 1
+
+        for item in subset:
+            product *= int(item)
+
+        if self.largest_product < product:
+            self.largest_product = product
 
     def getProduct(self):
         return self.largest_product
